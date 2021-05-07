521c500f4ccb56c96c26b2e9d599be5a0280e973
fumityuki <tokufumi0906@gmail.com>
2021-05-07 13:30:33 +0900

    new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Todo from "./Todo";
 
 function App() {
-  // 第11１変数がstate, 第２変数がstateを変化させる関数
+  // 第１変数がstate, 第２変数がstateを変化させる関数
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [finishedList, setFinishedList] = useState([]);
